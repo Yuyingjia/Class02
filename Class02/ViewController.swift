9f7e58aca79cf7b204486af6f8378c47a1fb7975
Yuyingjia <2735148454@qq.com>
2019-07-07 16:29:05 +0800

    我知道了。

--- Class02/ViewController.swift
+++ Class02/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var result : float_t = 10.2
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        var ce : Int
+        ce = 1
+        let de = add(a: 1, b: 2)
+        print(de)
+        print("我一定学得会 Swift")
+        
     }
-
+    func add(a : Int, b : Int) -> Int {
+        return a + b
+    }
 
 }
 
