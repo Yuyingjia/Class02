637c7f8f39a272a679edb3742565ff3d98d36e3b
Yuyingjia <2735148454@qq.com>
2019-07-08 17:07:23 +0800

    emmm

--- Class02/ViewController.swift
+++ Class02/ViewController.swift
@@ -10,20 +10,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lbMsg: UILabel!
     
     var result : float_t = 10.2
+    
+    @IBAction func btnTestClicked(_ sender: UIButton) {
+        
+        print("我按了啥？")
+        
+        lbMsg.text = "我要吃铜锣烧"
+        lbMsg.textColor = UIColor.green
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        var ce : Int
-        ce = 1
-        let de = add(a: 1, b: 2)
-        print(de)
-        print("我一定学得会 Swift")
+//        var ce : Int
+//        ce = 1
+//        let de = add(a: 1, b: 2)
+//        print(de)
+//        print("我一定学得会 Swift")
         
-    }
+        //我要去超市了
+        let 如果我去超市 = true
+        
+        //有橘子
+        let 如果看到橘子 = true
+        
+        
+        
+        
+        if 如果我去超市{
+            var 买西瓜的数量 = 1
+            if 如果看到橘子{
+                买西瓜的数量 = 10
+            }
+            print("我要买：\( 买西瓜的数量 )个西瓜")
+        }
+        
+//
+//        if( 如果我去超市 ){
+//
+//            var 要买几个西瓜 = 1
+//
+//
+//
+//         if(如果看到橘子){
+//            要买几个西瓜 = 10
+//           }
+//            print("买 \(要买几个西瓜) 个西瓜")
+//
+//        }
+  }
+    
     func add(a : Int, b : Int) -> Int {
+        
+        
         return a + b
     }
 
